5ca99d49535f7664c50552bcd606bd8eda835483
sinsoku <sinsoku.listy@gmail.com>
2016-01-12 20:47:47 +0900

    Implement class

---
+++ Source/HelloSwiftWorld.swift
@@ -0,0 +1,15 @@
+//
+//  HelloSwiftWorld.swift
+//  HelloSwiftWorld
+//
+//  Created by sinsoku on 2016/01/12.
+//
+//
+
+import Foundation
+
+class HelloSwiftWorld {
+    func run() {
+        print("Hello, World!")
+    }
+}
